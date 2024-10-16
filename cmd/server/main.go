afa37498f3ab8ed3db58bba0024670f883a8e4bf
longln <longlnofficial@gmail.com>
2024-10-16 19:57:42 +0700

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "github.com/longln/go-simplebank/internal/initialize"
+// import "github.com/longln/go-simplebank/internal/initialize"
 
-func main() {
-	initialize.Run()
-}
+// func main() {
+// 	initialize.Run()
+// }
